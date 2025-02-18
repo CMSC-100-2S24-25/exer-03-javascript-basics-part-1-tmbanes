f025e3ac06b68dbd97e5715509931f3bf739e4f3
tmbanes <tmbanes@up.edu.ph>
2025-02-18 15:04:21 +0800

    docs: updated README.md file and added documentation in index.js file

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 Name: Trisha Elaine M. Bañes
 Degree Program: BS Computer Science
+Section: CMSC 100-C1L
+Student Number: 2023-03451
+Date: Feb 18, 2025
+
+EXER #3
+
+Key Takeaways:
+Knowledge from previous courses (CMSC 12 and CMSC 22) helped with understanding the specifications of the exercise. That being said, it is important to know the basics of programming to help with future exercises :D
+
+References used for the exercise:
+1. https://www.geeksforgeeks.org/how-to-check-case-of-letter-in-javascript/
+    - used for checking characters in the password strings
+2. https://www.ascii-code.com/characters/0-9#:~:text=ASCII%20digits%20are%20the%20characters,represent%20numerical%20data%20in%20computers.
+    - used to get the ASCII values for the number
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function validatePassword(firstPass, secondPass){
     if(firstPass === secondPass){
         if(firstPass.length >= 8){
             for(var i = 0; i < firstPass.length; i++){
+                if(lowercaseCount >= 1 && uppercaseCount >= 1 && numberCount >= 1){
+                    return true;
+                } // checks if password already has met the conditions
+
+
+                // Used ASCII code to check characters inside the string
                 if(firstPass.charCodeAt(i) >= 48 && firstPass.charCodeAt(i) <= 57){
                     ++numberCount;
                     continue;
@@ -12,13 +18,11 @@ function validatePassword(firstPass, secondPass){
                 if(firstPass.charCodeAt(i) >= 65 && firstPass.charCodeAt(i) <= 90){
                     uppercaseCount++;
                     continue;
-                }if(firstPass.charCodeAt(i) >= 97 && firstPass.charCodeAt(i) <= 122){
+                }
+                if(firstPass.charCodeAt(i) >= 97 && firstPass.charCodeAt(i) <= 122){
                     lowercaseCount++;
                     continue;
                 }
-            }
-            if(lowercaseCount >= 1 && uppercaseCount >= 1 && numberCount >= 1){
-                return true;
             }
         }
     }
@@ -31,9 +35,10 @@ function validatePassword(firstPass, secondPass){
 function reverseString(password){
     var reversedString = "";
 
+    //Start from the end of password string then adds it in a new string
     for(var i = password.length - 1; i >= 0; i--){
         if(password[i] !== undefined){
-            reversedString = reversedString + password[i];
+            reversedString += password[i];
         } 
     }
     return reversedString;
@@ -44,17 +49,17 @@ function storePassword(name, firstPass, secondPass){
     const user = {
         name: name,
         newPassword: ""
-    }
+    } // user object
 
     if(validatePassword(firstPass, secondPass) === true){
         user.newPassword = reverseString(secondPass);
     }else{
         user.newPassword = firstPass;
     }
-
     return user;
 }
 
+// Input and Output
 console.log(storePassword("John", "Password1234", "Password1234"));
 console.log(storePassword("John", "Pass123", "Pass12345"));
 
