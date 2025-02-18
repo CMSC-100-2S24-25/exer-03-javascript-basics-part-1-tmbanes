da8cc42a9a9cf91fc6ac62f7d8736b9ae348800d
tmbanes <tmbanes@up.edu.ph>
2025-02-18 14:12:37 +0800

    feat: added store password function

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 function validatePassword(firstPass, secondPass){
     if(firstPass === secondPass){
-        if(firstPass.length === 8){
+        if(firstPass.length >= 8){
             return true;
         }
     }
-    return false;
+    else{
+        return false;
+    }
+    
 }
 
-var reversedString ="";
 
 function reverseString(password){
+    var reversedString = "";
+
     for(var i = password.length - 1; i >= 0; i--){
         if(password[i] !== undefined){
             reversedString = reversedString + password[i];
         console.log(password[i]);
         } 
     }
-    console.log(reversedString);
+
+    return reversedString;
 }
 
-reverseString("12345678");
+
+function storePassword(name, firstPass, secondPass){
+    const user = {
+        userName: name,
+        newPassword: ""
+    }
+
+    if(validatePassword(firstPass, secondPass) == true){
+        user.newPassword = reverseString(secondPass);
+        console.log("validated password");
+    }else{
+        console.log("not validated password");
+        user.newPassword = firstPass;
+    }
+
+    console.log(user);
+}
+
+storePassword("John", "Password1234", "Password1234");
